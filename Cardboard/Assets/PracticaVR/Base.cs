2e7905d7fbcb384631ffedbc3f0096cb842c3097
dgarciacosio <dgc643@gmail.com>
2019-01-09 16:51:21 +0100

    Ultimos cambios

---
+++ Cardboard/Assets/PracticaVR/Base.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+
+    public Material materialActivo, materialInactivo;
+    public float recogidaTiempo = 1f;
+    public int caidaTiempo = 500;
+    public Rigidbody gameObjectsRigidBody;
+
+    private float tiempo;
+    private bool sujeto = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        transform.SetParent(Camera.main.transform);
+        transform.localPosition = new Vector3(gameObject.transform.position.x, -1, gameObject.transform.position.z);
+        transform.localRotation = Quaternion.identity;
+        tiempo = Mathf.Infinity;
+        GetComponent<Renderer>().material = materialInactivo;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time - tiempo > recogidaTiempo)
+        {
+            //sujeto = true;
+            //interaccion();
+
+        }
+        if (sujeto == true)
+        {
+            caidaTiempo--;
+            if (caidaTiempo == 0)
+            {
+                sujeto = false;
+
+                caidaTiempo = 500;
+            }
+        }
+    }
+
+
+
+
+
+
+    public void activa(bool activado)
+    {
+
+        if (activado)
+        {
+
+            GetComponent<Renderer>().material = materialActivo;
+            Debug.Log("Activado");
+
+            tiempo = Time.time;
+
+        }
+
+        else
+        {
+            GetComponent<Renderer>().material = materialInactivo;
+            Debug.Log("Desactivado");
+
+            tiempo = Mathf.Infinity;
+
+        }
+
+    }
+
+
+    public void interaccion()
+    {
+
+
+
+
+        Debug.Log("Interaccion");
+    }
+
+}
---
+++ Cardboard/Assets/TerrainScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainScript : MonoBehaviour
+{
+    private Rigidbody gameObjectsRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObjectsRigidBody = gameObject.AddComponent<Rigidbody>();
+        gameObjectsRigidBody.mass = 1;
+        gameObjectsRigidBody.collisionDetectionMode = CollisionDetectionMode.Discrete;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
